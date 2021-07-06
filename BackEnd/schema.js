c085809a6e6f4f28f8ac45aa0e9747d6df357c85
Milo Boucher <milo.boucher@sigmalabs.xyz>
2021-07-06 19:38:43 +0100

    Create database schema and seeds

---
+++ BackEnd/schema.js
@@ -0,0 +1,46 @@
+import { config } from 'https://deno.land/x/dotenv/mod.ts'
+import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts"
+
+const DENO_ENV = Deno.env.get('DENO_ENV') ?? 'development'
+config({ path: `./.env.${DENO_ENV}`, export: true })
+
+const client = new Client(Deno.env.get("PG_URL"))
+await client.connect()
+
+await client.queryObject(
+  `CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    salted_password TEXT NOT NULL,
+    star_rating REAL,
+    date_of_birth DATE NOT NULL,
+    phone_number TEXT,
+    address TEXT,
+    city TEXT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+  )`
+)
+
+await client.queryObject(
+  `CREATE TABLE sessions (
+    uuid TEXT PRIMARY KEY,
+    user_id INTEGER,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+  )`
+)
+
+await client.queryObject(
+  `CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    is_available BOOLEAN NOT NULL DEFAULT TRUE,
+    category_id INTEGER,
+    owner_id INTEGER,
+    age_restriction INTEGER
+  )`
+)
