6fca285bf7980823bb727e0da7f0b8b763572bbb
Darshan Balasingam <darshan@Darshans-MacBook-Pro.local>
2021-07-07 11:58:49 +0100

    things page progress

--- FrontEnd/web-app/src/Things.js
+++ FrontEnd/web-app/src/Things.js
@@ -3,6 +3,22 @@ import { Redirect } from 'react-router-dom';
 import './index.css';
 
 class Things extends Component {
+
+        state = {
+            items: []
+        }
+    
+        async componentDidMount(){
+            const response = await fetch('http://localhost:8080/items')
+            //respose is making the request
+            const items =  await response.json()
+            //this.setState({items})
+            console.log(items)
+        }
+
+
+
+
     render() {
         return (
             <h1>{JSON.stringify(this.props.searchCriteria)}</h1>
@@ -11,3 +27,6 @@ class Things extends Component {
 }
 
 export default Things;
+
+
+
