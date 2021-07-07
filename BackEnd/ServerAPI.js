8f07ec85007f74493bdd76cd6db03ec26767dd83
Kyle Pearce <kylepearce@Kyles-MacBook-Pro.local>
2021-07-07 11:11:28 +0100

    Login logic

---
+++ BackEnd/ServerAPI.js
@@ -0,0 +1,76 @@
+import { Application } from 'https://deno.land/x/abc@v1.3.1/mod.ts';
+import { cors } from 'https://deno.land/x/abc@v1.3.1/middleware/cors.ts';
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+//ENVIROMENT VARIABLES
+import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
+const DENO_ENV = Deno.env.get('DENO_ENV') ?? 'development';
+config({ path: `./.env.${DENO_ENV}`, export: true });
+console.log(`Deno Enviroment: ${DENO_ENV}`);
+const client = new Client(Deno.env.get("PG_URL"));
+await client.connect();
+const PORT = Number(Deno.env.get("PORT"));
+
+const app = new Application();
+const headersWhitelist = [
+  "Authorization",
+  "Content-Type",
+  "Accept",
+  "Origin",
+  "User-Agent",
+];
+app.use(cors({ allowOrigins: [Deno.env.get("ALLOWED_ORIGINS")], allowHeaders: headersWhitelist, allowCredentials: true }));
+
+async function getCurrentUser(sessionIdArg)
+{
+  const sessionId = sessionIdArg;
+  const userId = (await client.queryObject("SELECT user_id FROM sessions WHERE uuid = $1", sessionId)).rows;
+  if(userId[0])
+  {
+    const userIdFinal = userId[0].user_id;
+    return (await client.queryObject("SELECT * FROM users WHERE id = $1", userIdFinal)).rows;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+app
+.get('/checkWhoIsSignedIn', async server => {
+    const sessionId = server.cookies['sessionId'];
+    const user = await getCurrentUser(sessionId);
+    if(user){
+      await server.json({ user });
+    }else {
+      await server.json(false);
+    }
+  })
+  .post("/login", async server => { 
+    const { email, password } = await server.body;
+    const user = (await client.queryObject("SELECT * FROM users WHERE email = $1", email)).rows;
+    const retrieveSaltedHash = (await client.queryObject("SELECT encrypted_password FROM users WHERE email = $1", email)).rows;
+    const existingSaltedHash = retrieveSaltedHash[0].encrypted_password;
+    const authenticated = await bcrypt.compare(password, existingSaltedHash);
+    if(authenticated) 
+    {  
+      const sessionId = v4.generate();
+      const insert = (await client.queryObject("INSERT INTO sessions (uuid, user_id, created_at) VALUES ($1, $2, NOW())", sessionId, user[0].id)).rows;
+      server.setCookie({
+        name: "sessionId", 
+        value: sessionId,
+        path: "/",
+        expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000 ),
+        sameSite: 'none',
+        secure: true
+      });
+      await server.json({  code: 200 });
+    } 
+    else 
+    {
+      await server.json({  code: 500  });
+    }
+  })
+  .start({ port: PORT })
+console.log(`Server running on http://localhost:${PORT}`);
