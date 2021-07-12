ac8af2a9df15e14b6b42fb5fad0ddfce559e5ad8
RobertScholey98 <45205578+RobertScholey98@users.noreply.github.com>
2021-07-12 15:05:42 +0100

    some more tweaks to the searchbar

--- FrontEnd/web-app/src/components/home/SearchBar.js
+++ FrontEnd/web-app/src/components/home/SearchBar.js
@@ -53,13 +53,16 @@ class SearchBar extends Component {
         const { item, dateFrom, dateTo, location } = this.state;
         return (
             <main>
-                <h1></h1>
                 <form type="submit" className="searchBar">
+                    <div>
                     <span><input type="text" name="item" id="item" value={item} onChange={(e) => this.handleChange(e)} placeholder='item' /></span>
                     <span><input type="date" name="dateFrom" id="dateFrom" value={dateFrom} onChange={(e) => this.handleChange(e)} placeholder='dateFrom' /></span>
                     <span><input type="date" name="dateTo" id="dateTo" value={dateTo} onChange={(e) => this.handleChange(e)} placeholder='dateTo' /></span>
                     <span><input type="text" name="location" id="location" value={location} onChange={(e) => this.handleChange(e)} placeholder='location' /></span>
-                    <span><button onClick={(e) => this.formSubmission(e)} >Click Me!</button></span>
+                    </div>
+                    <div>
+                    <input id='search_submit' type='submit' onClick={(e) => this.formSubmission(e)} value='GO'/>
+                    </div>
                 </form>
             </main>
         );
